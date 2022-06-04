b8701d7382c1c9c38d303741c406dfc65546aa2b
Jun-Gyu Lee <yjpress@hanmail.net>
2022-06-05 07:47:28 +0900

    dffggf

---
+++ test53/src/main/java/kr/didym/netprint/Test53Application.java
@@ -0,0 +1,13 @@
+package kr.didym.netprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test53Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test53Application.class, args);
+	}
+
+}
---
+++ test53/src/test/java/kr/didym/netprint/Test53ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.didym.netprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test53ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
